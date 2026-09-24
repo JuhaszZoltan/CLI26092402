bb7ca13eb80351c443499bab4a237f2a86003cb6
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-09-24 11:29:37 +0200

    Update Program.cs
    add loop

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ string name = Console.ReadLine() ?? throw new Exception("cannot be null");
 
 string hw = $"Hello, {name}!";
 
-foreach (var c in hw)
-{
-    Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
-    Console.Write(c);
+Console.Write("how many times?: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{    
+    foreach (var c in hw)
+    {
+        Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
+        Console.Write(c);
+    }
+    Console.Write('\n');
 }
 
 while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
