e084c65686c2dc9c1e840c5bb9bbe26684317435
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-09-24 11:06:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//https://git-scm.com/
+
+string hw = "Hello, World!";
+
+foreach (var c in hw)
+{
+    Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
+    Console.Write(c);
+}
+
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
